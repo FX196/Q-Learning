1e0a054d37eb6d515556c3f1a09cf8fb712d7e59
FX196 <mikechen212@foxmail.com>
2018-02-28 13:28:32 +0800

    added move

--- 2048/game_2048.py
+++ 2048/game_2048.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 EMPTY_CELL = "    "
 
@@ -9,6 +10,7 @@ class Game(object):
         self.board = get_board(size)
         self.score = 0
         self.game_over = False
+        self.new_cell()
 
     def display(self):
         print("Score: %d" % self.score)
@@ -20,20 +22,45 @@ class Game(object):
             print()
             print("-" * 21)
 
+    # add new cell with value of 2 or 4 to a random empty cell on the board
+    # returns: True if adding successful, False if unsuccessful i.e. no empty cells
+    def new_cell(self):
+        board = self.board
+        empty_cells = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if board[i][j] == EMPTY_CELL:
+                    empty_cells.append((i, j))
+        if empty_cells:
+            r, c = empty_cells[random.randint(0, len(empty_cells) - 1)]
+            board[r][c] = random.randint(1, 2) * 2
+            return True
+        else:
+            return False
+
+    # process action, 0 for up, 1 for down, 2 for left, 3 for right
+    # returns: True if move legal, false if otherwize
     def step(self, action):
-        moved = True
+        temp_board = []
         if action == "0":
-            pass
+            for col in transpose(self.board):
+                temp_board.append(reduce(col))
+                temp_board = transpose(temp_board)
         elif action == "1":
-            pass
+            for col in transpose(self.board):
+                temp_board.append(reduce(col[::-1])[::-1])
+                temp_board = transpose(temp_board)
         elif action == "2":
-            pass
+            for row in self.board:
+                temp_board.append(reduce(row))
         elif action == "3":
-            pass
+            for row in self.board:
+                temp_board.append(reduce(row[::-1])[::-1])
         else:
             print("Action invalid")
-        if moved:
-            new_cell(self)
+        if temp_board != self.board:
+            self.board = temp_board
+            self.new_cell()
             self.display()
 
 
@@ -48,30 +75,16 @@ def get_board(size):
     return board
 
 
-# add new cell with value of 2 or 4 to a random empty cell on the board
-# returns: True if adding successful, False if unsuccessful i.e. no empty cells
-def new_cell(data):
-    board = data.board
-    empty_cells = []
-    for i in range(data.size):
-        for j in range(data.size):
-            if board[i][j] == EMPTY_CELL:
-                empty_cells.append((i,j))
-    if empty_cells:
-        r, c = empty_cells[random.randint(0,len(empty_cells)-1)]
-        board[r][c] = random.randint(1, 2) * 2
-        return True
-    else:
-        return False
-
-
 # reduces row i.e. move and merge cells to the left
 # returns: list representing reduced row
 def reduce(row):
     ind = 0
     row_length = len(row)
+    row = copy.deepcopy(row)
     while ind < len(row)-1:
-        if row[ind+1] == EMPTY_CELL:
+        if row[ind] == EMPTY_CELL:
+            row.pop(ind)
+        elif row[ind+1] == EMPTY_CELL:
             row.pop(ind+1)
         elif row[ind] == row[ind+1]:
             row[ind] += row.pop(ind+1)
@@ -86,6 +99,7 @@ def reduce(row):
 # transposes board to simplify step process
 # returns: 2D list representing transposed board
 def transpose(board):
+    board = copy.deepcopy(board)
     for i in range(len(board)):
         for j in range(i):
             board[i][j], board[j][i] = board[j][i], board[i][j]
@@ -98,3 +112,4 @@ if __name__ == "__main__":
     while not data.game_over:
         action = input()
         data.step(action)
+
